3cb5bae990aa5e206ea9e7a5a9421286be217ad1
Sina <sinadavachi1324@gmail.com>
2022-10-09 05:45:51 +0200

    Added resting mode

--- Jetson_Control/python_control/jetson_control.py
+++ Jetson_Control/python_control/jetson_control.py
@@ -12,11 +12,14 @@ PORT = "/dev/ttyUSB0"
 # Baud rate of local module.
 BAUD_RATE = 230400
 
+# Maestro Initialisation
 servo = maestro.Controller()
 target = 0
 min_pos = 800*4
 max_pos = 2000*4
 channel = 0
+rnge_s = 7996-4032
+rnge_b = 7360-4992
 
 i = 0
 header = False
@@ -29,6 +32,12 @@ q_lock = threading.Lock()
 curr_lock = threading.Lock()
 manual_data = 0
 selected_mode_handler = None
+
+def servo_s_conversion(percentage):
+    return ((percentage+100)/200 * rnge_s) + 4032
+
+def servo_b_conversion(percentage):
+    return ((percentage+100)/200 * rnge_b) + 4992
 
 def data_receive_callback(xbee_message):
     global header, CLASS, SIZE, manual_data
@@ -80,29 +89,42 @@ def data_receive_callback(xbee_message):
                 manual_data = var
                 selected_mode_handler(manual_data)
 
-#command handlers              
+# Command Handler
 def manual_control(data):
-    global servo, target
-    rnge_s = 7996-4032
-    rnge_b = 7360-4992
+    global servo, target, rnge_s, rnge_b
     print(data)
     data = list(data)
     for x in range(2):
-        data[x] = ((data[x]+100)/200 * rnge_s) + 4032
-    data[2] =  ((data[2]+100)/200 * rnge_b) + 4992
-    
+        data[x] = servo_s_conversion(data[x])
+    data[2] = servo_b_conversion(data[2])
+
     steering, thrust, brk, tail = [int(x) for x in data]
-    
-    servo.setTarget(channel, steering)
-    servo.setTarget(channel+1, thrust)
-    servo.setTarget(channel+2, brk)
-    
-        
+    servos = [steering, thrust, brk]
+    for x in range(3):
+        servo.setTarget(channel+x, servos[x])
+
 def auto_control(data):
     pass
 
 def control_loop_control(data):
     pass
+
+def rest_control(placeholder):
+    global servo, target, rnge_s, rnge_b
+    data = [0,0,0,0]
+    for x in range(2):
+        data[x] = servo_s_conversion(data[x])
+    data[2] = servo_b_conversion(data[2])
+    
+    steering, thrust, brk, tail = [int(x) for x in data]
+    for x in range(3):
+        servo.setSpeed(channel+x, 5)
+    servos = [steering, thrust, brk]
+    for x in range(3):
+        servo.setTarget(channel+x, servos[x])
+    time.sleep(1)
+    for x in range(3):
+        print("Servo " + str(x) + " at " + str(servo.getPosition(channel+x)))
 
 def kill():
     pass
@@ -118,9 +140,12 @@ def operating_mode(data):
     mode_map = {
         0x0: manual_control,
         0x1: auto_control,
-        0x2: control_loop_control
+        0x2: control_loop_control,
+        0x3: rest_control
     }
     selected_mode_handler = mode_map[data]
+    if data in [1, 3]:
+        selected_mode_handler(data)
 
 class_dict = {0x0:operating_mode}
 
@@ -130,21 +155,22 @@ def main():
     print(" |         XBee Receive Data Sample        |")
     print(" +-----------------------------------------+\n")
 
-    #initialise servo
+    # initialise servo
     device = XBeeDevice(PORT, BAUD_RATE)
     servo.setAccel(channel, 4)  # set servo 0 acceleration to 4
     servo.setSpeed(channel, 0)  # set speed of servo 0
-    
+
     try:
         device.open()
         device.add_data_received_callback(data_receive_callback)
         while (True):
             if (len(command_q) > 0):
-                print("Class: " + str(command_q[-1][0]))
-                print(command_q[-1][1])
+                # changed command_q[-1][0] to [0][0] NB
+                print("Class: " + str(command_q[0][0]))
+                print(command_q[0][1])
                 cls, data = command_q.pop(0)
                 class_dict[cls](data)
-                
+
     finally:
         if device is not None and device.is_open():
             device.close()
