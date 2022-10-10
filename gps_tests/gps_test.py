5d7779d8f272f17fbc5d74e5cafb4e383baa69fb
Sina <sinadavachi1324@gmail.com>
2022-10-10 04:19:08 +0200

    Added helpful commands for bno055 installation

--- gps_tests/gps_test.py
+++ gps_tests/gps_test.py
@@ -4,15 +4,15 @@ from time import sleep
 with serial.Serial('/dev/gps2', baudrate=9600, timeout=1) as ser:
     while True:
         sleep(0.1)
-        
+
         line = ser.readline()   # read a '\n' terminated line
         print(line)
-        line = line.decode()        
+        line = line.decode()
         if 'GNVTG' not in line:
             continue
-        
+
         line = line.split(',')
-        
+
         print(line[6])
-        print(line[8])  
-        
+        print(line[8])
+
--- gps_tests/uart.py
+++ gps_tests/uart.py
@@ -1,7 +1,7 @@
 import serial
 from time import sleep
 
-with serial.Serial('/dev/ttyACM0', 
+with serial.Serial('/dev/ttyACM0',
                    baudrate=9600, timeout=1) as ser:
     while True:
         line = ser.readline()   # read a '\n' terminated line
